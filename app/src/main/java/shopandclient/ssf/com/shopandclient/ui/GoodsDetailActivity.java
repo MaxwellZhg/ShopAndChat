6632e40c9886c18142f6af09d8e52d8e6737f5b1
maxwell_smith <maxwell_smith@163.com>
2019-10-10 07:39:59 +0800

    在家im..本地修改群名，，IMnickname

--- app/src/main/java/shopandclient/ssf/com/shopandclient/ui/GoodsDetailActivity.java
+++ app/src/main/java/shopandclient/ssf/com/shopandclient/ui/GoodsDetailActivity.java
@@ -169,11 +169,6 @@ public class GoodsDetailActivity extends BaseActivity implements BaseBiz, Observ
         itemtips.add("商品详情");
         itemtips.add("商品参数");
         itemtips.add("评价");
-        bannerlist.add("http://i9.hexunimg.cn/2013-07-05/155842064.jpg");
-        bannerlist.add("http://hbimg.b0.upaiyun.com/fc4e0017928cd9281e13a84c025b5277e5314d2c247e8-VOlLH3_fw658");
-        bannerlist.add("http://i6.hexunimg.cn/2013-07-05/155842061.jpg");
-        bannerlist.add("http://s10.sinaimg.cn/mw690/006hikKrzy7sly9tEBb49&amp");
-        bannerlist.add("http://i6.hexunimg.cn/2013-07-05/155842061.jpg");
         banner.setImages(bannerlist).setImageLoader(glideImageLoader).start();
         banner.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -239,6 +234,10 @@ public class GoodsDetailActivity extends BaseActivity implements BaseBiz, Observ
             public void onResponse(Call<ProductInfo> call, Response<ProductInfo> response) {
                 if (response.body().getCode() == 200) {
                     data = response.body().getData();
+                   for(int i=0;i<data.getProImg().size();i++){
+                       bannerlist.add(data.getProImg().get(i).getFileImg());
+                   }
+                    banner.setImages(bannerlist).setImageLoader(glideImageLoader).start();
                     attrs.addAll(response.body().getData().getProAttrType());
                     typevalue1 = response.body().getData().getProAttrType().get(0).getProAttrTypeValue().get(0).getId();
                     typevalue2 = response.body().getData().getProAttrType().get(1).getProAttrTypeValue().get(0).getId();
